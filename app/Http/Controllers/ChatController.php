454a09c56cb47192743bd8517df5a98f785378b0
kthurman59 <kthurman59@gmail.com>
2024-09-17 14:52:30 -0500

    chat controller init

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\MessageSent;
+use App\Models\Message;
 use Illuminate\Http\Request;
 
 class ChatController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('chat');
+    }
+
+    public function sendMessage(request $request)
+    {
+        $request->validate([
+            'content' => 'required|string'
+        ]);
+
+        $message = auth()->user()->messages()->create([
+            'content' => $request->content
+        ]);
+
+        broadcast(new MessageSent($message))->toOthers();
+
+        return response()->json($message);
+    }
+
+    public function getMessages()
+    {
+        return Message::with('user')->latest()->limit(50)->get()->reverse();
+    }
 }
